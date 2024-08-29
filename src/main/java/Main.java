224b28d312733cee805d2ffc83278c90e92c6d82
20211080030030 <soaresgiovanna815@gmail.com>
2024-08-29 12:10:23 -0300

    aula 29 de agosto

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import model.Pagamento;
 
 import java.util.Date;
@@ -5,11 +8,17 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pagamentosPU");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
         Pagamento pg= new Pagamento();
-        pg.setId(11);
         pg.setData(new Date());
         pg.setValor(500);
         System.out.println(pg);
+        entityManager.persist(pg);
+        entityManager.getTransaction().commit();
+        entityManagerFactory.close();
+        entityManager.close();
     }
 
 
